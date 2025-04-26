b8dce37ff5f79ccc652fe406a60d1030def94b19
mikhail10101 <mikhail.borbe.lg@gmail.com>
2025-04-25 23:29:27 -0700

    profile

--- models/userModel.js
+++ models/userModel.js
@@ -2,9 +2,19 @@ export class User {
     UID;
     firstName;
     lastName;
-    constructor (UID, firstName, lastName) {
+    email;
+    phone;
+    profession;
+    districtID;
+    bio;
+    constructor (UID, firstName, lastName, email, phone, profession, districtID, bio) {
         this.UID = UID;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.profession = profession;
+        this.districtID = districtID;
+        this.bio = bio;
     }
 }
