73cbc10eefa638f213f31486e9d8a52385fa849d
XxKillerDemonxX <149117791+AdamTan12@users.noreply.github.com>
2025-04-26 23:18:52 -0700

    fixed?

--- apis/signature.js
+++ apis/signature.js
@@ -5,13 +5,41 @@ import { signatureConverter } from "../converters/signatureConverter";
 export async function getSignatures(id) {
     let signatures = [];
     try {
-        const signatureCollections = collection(db, 'signatures').withConverter(userConverter);
-        const q = query(signatureCollections, where(initiativeUID, '==', id));
+        const signatureCollections = collection(db, 'signatures').withConverter(signatureConverter);
+        const q = query(signatureCollections, where("initiativeUID", '==', id));
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
             signatures.push(doc.data());
         })
         return signatures;
+    }
+    catch(e) {
+        throw(e);
+    }
+}
+export async function getSignatureByUserUID(userUID) {
+    try {
+        const signatureCollections = collection(db, 'signatures').withConverter(signatureConverter);
+        const q = query(signatureCollections, where("userUID", '==', userUID));
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.empty)
+            return null;
+        const signature = querySnapshot.docs[0].data();
+        return signature;
+    }
+    catch(e) {
+        throw(e);
+    }
+}
+export async function getSignatureByUserUIDandInitiativeUID(userUID, IUID) {
+    try {
+        const signatureCollections = collection(db, 'signatures').withConverter(signatureConverter);
+        const q = query(signatureCollections, where("userUID", '==', userUID), where("initiativeUID", '==', IUID));
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.empty)
+            return null;
+        const signature = querySnapshot.docs[0].data();
+        return signature;
     }
     catch(e) {
         throw(e);
--- models/signatureModel.js
+++ models/signatureModel.js
@@ -1,9 +1,11 @@
 export class Signature {
     UID;
+    userUID;
     initiativeUID;
     signature;
-    constructor(UID, initiativeUID, signature) {
+    constructor(UID, userUID, initiativeUID, signature) {
         this.UID = UID;
+        this.userUID = userUID;
         this.initiativeUID = initiativeUID;
         this.signature = signature;
     }
